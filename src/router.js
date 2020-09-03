bc5ae8bccd6d961f5ef1a1731a0d43d58ccc16c8
James Kwok <jamesuni2018@gmail.com>
2020-09-04 10:58:39 +1200

    Added basic handling for scheduled trips and added buttons for uploading data.

--- src/router.js
+++ src/router.js
@@ -3,12 +3,14 @@ import VueRouter from "vue-router";
 import Login from "@/components/Login";
 import Profile from "@/components/Profile";
 import HelloWorld from "@/components/HelloWorld";
+import AddTrip from "./components/AddTrip";
 
 const routes = [
     {path: "/", component: HelloWorld, name: "helloworld"},
     {path: "/login", component: Login, name: "login"},
-    {path: "/profile", component: Profile, name: "profile"}
-]
+    {path: "/profile", component: Profile, name: "profile"},
+    {path: "/addTrip", component: AddTrip, name: "addTrip"}
+];
 
 const router = new VueRouter(
     {
