41dbf1e9f9aa413f03852052769e15a41b472009
Lydia Looi <erkfir@icloud.com>
2020-09-04 11:01:08 +1200

    nav bar xd #story[help me]

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <NavBar></NavBar>
+
     <router-view/>
 
   </div>
@@ -7,20 +9,17 @@
 
 <script>
 
+import NavBar from "@/components/NavBar";
 export default {
   name: 'App',
   components: {
+    NavBar
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
 }
 </style>
--- src/router.js
+++ src/router.js
@@ -2,10 +2,12 @@ import VueRouter from "vue-router";
 
 import Login from "@/components/Login";
 import HelloWorld from "@/components/HelloWorld";
+import Profile from "@/components/Profile";
 
 const routes = [
     {path: "/", component: HelloWorld, name: "helloworld"},
-    {path: "/login", component: Login, name: "login"}
+    {path: "/login", component: Login, name: "login"},
+    {path: "/profile", component: Profile, name: "profile"}
 ]
 
 const router = new VueRouter(
