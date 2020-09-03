69e82b5f30e52cf6b5c23751249237b9a0f26b4f
Hamish Thwaites <hamisht99@gmail.com>
2020-09-04 11:43:48 +1200

    Leaderboard has entries now.  Everything is hardcoded, as this is best practice ;)

--- src/components/Leaderboard.vue
+++ src/components/Leaderboard.vue
@@ -1,12 +1,31 @@
 <template>
     <div>
         <p>Leaderboard</p>
+      <div>
+        <br><br><br>
+        <ul>
+          <li>User: Leaf Points</li>
+          <br>
+          <li v-for="item in items" :key="item.leafPoints">
+            {{ item.userName }}: {{ item.leafPoints}}
+          </li>
+        </ul>
+      </div>
     </div>
+
 </template>
 
 <script>
     export default {
-        name: "Leaderboard"
+        name: "Leaderboard",
+
+      data() {
+          return {
+            items: [{leafPoints: 70, userName: "Nishi"},
+              {leafPoints: 50, userName: "Ryzaak"},
+              {leafPoints: 2, userName: "Jamie"}],
+          }
+      }
     }
 </script>
 
