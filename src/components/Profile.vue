a2916e71d456b50a95ebfd0e0b10c87ee6f56c18
James Kwok <jamesuni2018@gmail.com>
2020-09-04 10:32:33 +1200

    Created profile page placeholders and added the route to router.js

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,6 +1,22 @@
 <template>
     <div>
-        <p>Profile page</p>
+        <div>
+        <img src="https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Background image">
+        <img src="https://cdn2.iconfinder.com/data/icons/people-80/96/Picture1-512.png" alt="Profile Image">
+        <h1>John Smith</h1>
+        </div>
+        <div class="leafPoints">
+            <h2>Leaf Points</h2>
+        </div>
+        <div class="badges">
+            <h2>Badges</h2>
+        </div>
+        <div class="stats">
+            <h1>Stats</h1>
+            <h2>Distance Travelled</h2>
+            <h3>Map</h3>
+        </div>
+
     </div>
 </template>
 
@@ -13,3 +29,4 @@
 <style scoped>
 
 </style>
+
--- src/router.js
+++ src/router.js
@@ -1,11 +1,13 @@
 import VueRouter from "vue-router";
 
 import Login from "@/components/Login";
+import Profile from "@/components/Profile";
 import HelloWorld from "@/components/HelloWorld";
 
 const routes = [
     {path: "/", component: HelloWorld, name: "helloworld"},
-    {path: "/login", component: Login, name: "login"}
+    {path: "/login", component: Login, name: "login"},
+    {path: "/profile", component: Profile, name: "profile"}
 ]
 
 const router = new VueRouter(
