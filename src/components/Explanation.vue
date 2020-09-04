d22edad7604924e863892c508b5e626aad4b1be4
Lydia Looi <erkfir@icloud.com>
2020-09-04 18:38:53 +1200

    basic css to explanation

--- src/components/Explanation.vue
+++ src/components/Explanation.vue
@@ -1,27 +1,37 @@
 <template>
-<div>
-    <nav-bar></nav-bar>
-    <br><br><br>
-    <p>How it Works:</p>
     <div>
-        <p>
-            Leaf Points are calculated by finding the amount of carbon dioxide you have avoided producing.
-            We calculate the amount of carbon dioxide you would have produced using a car, and subtract the amount you actually produced.
-            The remaining amount of carbon dioxide (in grams/100) is then added to your leaf points.
-        </p>
-        <p>
-            (Note that we assume 8L/100km fuel efficiency for cars and 38 g/km for a bus passenger)
-        </p>
+        <nav-bar></nav-bar>
+        <div class="pt-32">
+            <div class="bg-white general-container-rounded mx-6">
+                <p class="text-gray-800 text-2xl border-b-2 mb-4">How it Works</p>
+                <div class="text-sm">
+                    <p class="mb-2">Leaf Points are calculated by finding the amount of carbon dioxide you have avoided
+                        producing.</p>
+                    <p class="mb-2">
+                        We calculate the amount of carbon dioxide you would have produced using a car, and subtract
+                        the
+                        amount you actually produced.</p>
+                    <p class="mb-2">
+                        The remaining amount of carbon dioxide (in grams/100) is then added to your leaf points.
+                    </p>
+                    <p>
+                        Note that we assume 8L/100km fuel efficiency for cars and 38 g/km for a bus passenger
+                    </p>
+                </div>
+            </div>
+
+        </div>
+
     </div>
-</div>
 </template>
 
 <script>
-import NavBar from "./NavBar";
-export default {
-name: "Explanation.vue",
-    components: {NavBar}
-}
+    import NavBar from "./NavBar";
+
+    export default {
+        name: "Explanation.vue",
+        components: {NavBar}
+    }
 </script>
 
 <style scoped>
