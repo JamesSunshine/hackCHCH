65738a1103846aae060ff6986e9478baf0cf07b3
Lydia Looi <erkfir@icloud.com>
2020-09-04 18:42:22 +1200

    added button to my badges

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -73,9 +73,6 @@
                   class="w-6 h-6"
           >
 
-<!--              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">-->
-              <!--  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />-->
-              <!--</svg>-->
   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
         d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>
 
@@ -83,6 +80,30 @@
         </span>
         <span>Profile</span></span
             >
+
+
+            <span
+                    @click="isOpen = false"
+                    v-on:click="goToBadgeProgress"
+                    class="flex items-center p-4 hover:bg-indigo-500 hover:text-white "
+            ><span class="mr-2">
+          <svg
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  class="w-6 h-6"
+          >
+
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+        d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
+          </svg>
+        </span>
+        <span>My Badges</span></span
+            >
+
             <span
                     @click="isOpen = false"
                     v-on:click="goToTeam"
@@ -254,6 +275,12 @@
             goToLogout() {
                 this.$router.push({name: "login"})
             },
+
+            goToBadgeProgress() {
+                this.$router.push({name: "badges"})
+            }
+
+
 
 
         },
